c91e2200e9e6ec54027494528158773a65a478dd
AdrianHernandezEstrella <89058467+AdrianHernandezEstrella@users.noreply.github.com>
2023-05-31 03:40:28 -0600

    COMMIT FINAL

---
+++ src/main/java/com/consume/apinew/models/ProduccionModelo.java
@@ -0,0 +1,49 @@
+package com.consume.apinew.models;
+
+public class ProduccionModelo {
+	private String actividad;
+    private String codigo;
+    private int id;
+    private String nombre;
+    private String responsable;
+    private String uso;
+    
+	public String getActividad() {
+		return actividad;
+	}
+	public void setActividad(String actividad) {
+		this.actividad = actividad;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getResponsable() {
+		return responsable;
+	}
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+	public String getUso() {
+		return uso;
+	}
+	public void setUso(String uso) {
+		this.uso = uso;
+	}
+    
+    
+}
